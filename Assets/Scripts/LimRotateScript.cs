575499bddefa70b3da458d51e2358457dff546ec
JBorders3010 <bordersjb@siu.edu>
2017-05-10 09:26:01 -0400

    Added developer splash screen

---
+++ Assets/Scripts/LimRotateScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimRotateScript : MonoBehaviour {
+    public Vector3 axis;
+    public float speed;
+    public GameObject target;
+    private bool stop;
+
+    void Start()
+    {
+        stop = false;
+    }
+    
+    void Update()
+    {
+        if (transform.position.Equals(target.transform.position) && !stop)
+        {
+            stop = true;
+        }
+        if (!(stop && transform.rotation.Equals(Quaternion.identity)))
+        {
+            transform.RotateAround(transform.position, axis, speed * Time.deltaTime);
+        }
+        if (stop && Quaternion.Dot(transform.rotation, Quaternion.identity) >= 0.9999f)
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
