6fa9331859ca30374f746a18d67e636c3b824b63
JBorders3010 <bordersjb@siu.edu>
2017-05-10 10:47:21 -0400

    Implemented developer splash screen

--- Assets/Scripts/LimRotateScript.cs
+++ Assets/Scripts/LimRotateScript.cs
@@ -1,21 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LimRotateScript : MonoBehaviour {
     public Vector3 axis;
     public float speed;
     public GameObject target;
     private bool stop;
+    private int count;
 
     void Start()
     {
         stop = false;
+        count = 0;
     }
     
     void Update()
     {
-        if (transform.position.Equals(target.transform.position) && !stop)
+        if ((Vector3.Dot(transform.position, target.transform.position) >= .6f * Vector3.SqrMagnitude(target.transform.position)) && !stop)
         {
             stop = true;
         }
@@ -23,9 +24,14 @@ public class LimRotateScript : MonoBehaviour {
         {
             transform.RotateAround(transform.position, axis, speed * Time.deltaTime);
         }
-        if (stop && Quaternion.Dot(transform.rotation, Quaternion.identity) >= 0.9999f)
+        if (stop && Quaternion.Dot(transform.rotation, Quaternion.identity) >= 0.995f)
         {
             transform.rotation = Quaternion.identity;
         }
+        if (count >= 725)
+        {
+            SceneManager.LoadScene("MainScreen");
+        }
+        count++;
     }
 }
